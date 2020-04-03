0c2e168eaf58a2bb94d84081bf8ea5edadb4e651
Adrian Tineo <adrian@adriantineo.com>
2020-04-03 19:07:45 +0200

    Add working implementation (but too slow)

--- Leetcode_30_day_challenge_April_2020.playground/Pages/03 -Maximum Subarray.xcplaygroundpage/Contents.swift
+++ Leetcode_30_day_challenge_April_2020.playground/Pages/03 -Maximum Subarray.xcplaygroundpage/Contents.swift
@@ -20,9 +20,44 @@
 
 import Foundation
 
+func myPrint(_ arg: Any) {
+    //print(arg)
+}
+
 class Solution {
     func maxSubArray(_ nums: [Int]) -> Int {
-        return 0
+        assert(nums.count > 0)
+
+        return maxSubArrayA(nums)
+    }
+    
+    // Time: O(nlogn)
+    // Space: O(n)
+    private func maxSubArrayA(_ nums: [Int]) -> Int {
+        var maxSum = nums[0]
+        myPrint(maxSum)
+        var i = 0
+        while i < nums.count {
+            var j = i + 1
+            var newSum = nums[i]
+            if newSum > maxSum {
+                maxSum = newSum
+                myPrint("update max to \(maxSum)")
+            }
+            myPrint("i: \(i), j: \(j), newSum: \(newSum)")
+            while j < nums.count {
+                newSum += nums[j]
+                myPrint("i: \(i), j: \(j), newSum: \(newSum)")
+                if newSum > maxSum {
+                    maxSum = newSum
+                    myPrint("update max to \(maxSum)")
+                }
+                j += 1
+            }
+            myPrint("")
+            i += 1
+        }
+        return maxSum
     }
 }
 
@@ -34,6 +69,63 @@ class TestCase: XCTestCase {
         let actualOutput = solution.maxSubArray(input)
         XCTAssertEqual(actualOutput, expectedOutput)
     }
+    
+    @objc func testB() {
+        let solution = Solution()
+        let input = [4,-1,2,1,-5]
+        let expectedOutput = 6
+        let actualOutput = solution.maxSubArray(input)
+        XCTAssertEqual(actualOutput, expectedOutput)
+    }
+    
+    @objc func testC() {
+        let solution = Solution()
+        let input = [4,-1,2]
+        let expectedOutput = 5
+        let actualOutput = solution.maxSubArray(input)
+        XCTAssertEqual(actualOutput, expectedOutput)
+    }
+    
+    @objc func testD() {
+        let solution = Solution()
+        let input = [4,-1,2,-5]
+        let expectedOutput = 5
+        let actualOutput = solution.maxSubArray(input)
+        XCTAssertEqual(actualOutput, expectedOutput)
+    }
+    
+    @objc func testE() {
+        let solution = Solution()
+        let input = [4,-1,2,-5,7]
+        let expectedOutput = 7
+        let actualOutput = solution.maxSubArray(input)
+        XCTAssertEqual(actualOutput, expectedOutput)
+    }
+    
+    @objc func testF() {
+        let solution = Solution()
+        let input = [4,-1,2,1,-5,2,3,4]
+        let expectedOutput = 10
+        let actualOutput = solution.maxSubArray(input)
+        XCTAssertEqual(actualOutput, expectedOutput)
+    }
+    
+    @objc func testG() {
+        let solution = Solution()
+        let input = [-1]
+        let expectedOutput = -1
+        let actualOutput = solution.maxSubArray(input)
+        XCTAssertEqual(actualOutput, expectedOutput)
+    }
+    
+    @objc func testH() {
+        let solution = Solution()
+        let input = [-2,1]
+        let expectedOutput = 1
+        let actualOutput = solution.maxSubArray(input)
+        XCTAssertEqual(actualOutput, expectedOutput)
+    }
+    
 }
 
 TestCase()
