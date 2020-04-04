012f7983b4fc4656012f3d7c3e5d4b25157e56db
Adrian Tineo <adrian@adriantineo.com>
2020-04-04 17:52:23 +0200

    Add solution to problem 4

--- Leetcode_30_day_challenge_April_2020.playground/Pages/04-Move Zeroes.xcplaygroundpage/Contents.swift
+++ Leetcode_30_day_challenge_April_2020.playground/Pages/04-Move Zeroes.xcplaygroundpage/Contents.swift
@@ -21,7 +21,27 @@ import Foundation
 
 class Solution {
     func moveZeroes(_ nums: inout [Int]) {
+        _ = nums.halfStablePartition(isSuffixElement: { $0 == 0})
         return
+    }
+}
+
+extension MutableCollection {
+    /// Moves all elements satisfying `isSuffixElement` into a suffix of the collection,
+    /// returning the start position of the resulting suffix.
+    ///
+    /// - Complexity: O(n) where n is the number of elements.
+    mutating func halfStablePartition(isSuffixElement: (Element) -> Bool) -> Index {
+        guard var i = firstIndex(where: isSuffixElement) else { return endIndex }
+        var j = index(after: i)
+        while j != endIndex {
+            if !isSuffixElement(self[j]) {
+                swapAt(i, j)
+                formIndex(after: &i)
+            }
+            formIndex(after: &j)
+        }
+        return i
     }
 }
 
@@ -30,6 +50,30 @@ class TestCase: XCTestCase {
         let solution = Solution()
         var input = [0,1,0,3,12]
         let expectedOutput = [1,3,12,0,0]
+        solution.moveZeroes(&input)
+        XCTAssertEqual(input, expectedOutput)
+    }
+    
+    @objc func testB() {
+        let solution = Solution()
+        var input = [0]
+        let expectedOutput = [0]
+        solution.moveZeroes(&input)
+        XCTAssertEqual(input, expectedOutput)
+    }
+    
+    @objc func testC() {
+        let solution = Solution()
+        var input = [1,3,12,0,0]
+        let expectedOutput = [1,3,12,0,0]
+        solution.moveZeroes(&input)
+        XCTAssertEqual(input, expectedOutput)
+    }
+    
+    @objc func testD() {
+        let solution = Solution()
+        var input = [0,-1,5,3,0]
+        let expectedOutput = [-1,5,3,0,0]
         solution.moveZeroes(&input)
         XCTAssertEqual(input, expectedOutput)
     }
