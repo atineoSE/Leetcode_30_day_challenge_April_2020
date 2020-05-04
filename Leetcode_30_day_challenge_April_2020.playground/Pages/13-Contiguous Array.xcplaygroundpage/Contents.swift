d36361d8a7e5b17f73f4821441d1f34fbbb7be6a
Adrian Tineo <adrian@adriantineo.com>
2020-05-04 20:28:24 +0200

    Add new solution for problem 13 (with python reference)

--- Leetcode_30_day_challenge_April_2020.playground/Pages/13-Contiguous Array.xcplaygroundpage/Contents.swift
+++ Leetcode_30_day_challenge_April_2020.playground/Pages/13-Contiguous Array.xcplaygroundpage/Contents.swift
@@ -53,28 +53,33 @@ struct LazyRangeGenerator {
     }
 }
 
+func myPrint(_ arg: Any) {
+    print(arg)
+}
+
 class Solution {
     
     func qualifies(_ nums: ArraySlice<Int>) -> Bool {
         return nums.reduce(0, +) == nums.count/2
     }
     
-    func findMaxLength(_ nums: [Int]) -> Int {
+    // Time: O(n^3): generates all subarrays O(N^2) and reduce each O(N)
+    // Space: O(1)
+    func findMaxLengthOO(_ nums: [Int]) -> Int {
         guard nums.count > 0 else { return 0 }
         
         var lazyRangeGenerator = LazyRangeGenerator(low: 0, high: nums.count-1)
-        var currentLength = nums.count
+        var currentLength = nums.count  // begin with largest lenght so as to exit early
         while currentLength > 1 {
             guard currentLength % 2 == 0 else {
                 currentLength -= 1
                 continue
             }
-
             lazyRangeGenerator.length = currentLength
             while let range = lazyRangeGenerator.next() {
                 let subarray = nums[range]
                 if qualifies(subarray) {
-                    return subarray.count
+                    return subarray.count   // early exit (doesn't help with asymptotic complexity though
                 }
             }
             currentLength -= 1
@@ -82,9 +87,62 @@ class Solution {
 
         return 0
     }
+    
+    
+    // Time: O(n)
+    // Space: O(n^3)
+    // Key insight: graph of adding up or substracting numbers, then keeping indices for same sum
+    // which indicates the length of the subarray
+    // Also initializing the dict to contain the first entry and updating the index before registering
+    func findMaxLengthWithDict(_ nums: [Int]) -> Int {
+        guard nums.count > 0 else { return 0 }
+        
+        var sumToIndicesDict: [Int: (Int, Int?)] = [0:(0,nil)]
+        
+        func register(sum: Int, newIndex: Int) {
+            if let (i, _) = sumToIndicesDict[sum] {
+                sumToIndicesDict[sum] = (i, newIndex)
+            } else {
+                sumToIndicesDict[sum] = (newIndex, nil)
+            }
+        }
+        
+        func getLength(indices: (Int,Int?)) -> Int? {
+            if let j = indices.1 {
+                return j - indices.0
+            } else {
+                return nil
+            }
+        }
+        
+        var i = 0
+        var sum = 0
+        while i < nums.count {
+            myPrint(sumToIndicesDict)
+            sum  = nums[i] == 0 ? sum - 1 : sum + 1
+            i += 1
+            register(sum: sum, newIndex: i)
+        }
+        
+        myPrint(sumToIndicesDict)
+        
+        // Only qualifying substrings, i.e. they have a second index
+        let lengths = sumToIndicesDict.values.compactMap { indices in
+            return getLength(indices: indices)
+        }
+
+        return lengths.max() ?? 0
+    }
+    
+    func findMaxLength(_ nums: [Int]) -> Int {
+        return findMaxLengthWithDict(nums)
+        //return findMaxLengthOO(nums)
+    }
 }
 
+
 class TestCase: XCTestCase {
+
     @objc func testDoesNotQualifyA() {
         let solution = Solution()
         let input = [0,0]
@@ -184,6 +242,7 @@ class TestCase: XCTestCase {
         let actualOutput = solution.findMaxLength(input)
         XCTAssertEqual(actualOutput, expectedOutput)
     }
+
     
     @objc func testH() {
         let solution = Solution()
